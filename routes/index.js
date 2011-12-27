ee127556d1b4d4f29f72aa530758925289ce9569
Alexandre Rimthong <rimthong@gmail.com>
2011-12-26 19:22:15 -0500

    Interface fixes, prepped for heroku

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
  * GET home page.
  */
 var openid = require('openid');
+var user;
 var Activity = require('../models/activity.js');
 var extensions = [new openid.UserInterface(), 
                   new openid.SimpleRegistration(
@@ -30,36 +31,37 @@ var relyingParty = new openid.RelyingParty(
     false, // Strict mode
     extensions); // List of extensions to enable and include
 
-
 exports.index = function(req, res){
-  console.log("invoking index");
-  res.render('login', { title: 'Express' });
+  res.render('login', { title: 'Please identify yourself' });
 };
 
 exports.activity = function(req, res){
-  console.log("invoking activities");
   relyingParty.verifyAssertion(req, function(error, result){
     if(error){
-      console.log('auth failed, error:'+error);
+      console.log('auth failed activity, error:'+JSON.stringify(error));
+      res.render('login', {title: 'Authentication Failed'});
     } else {
-      console.log('auth works, result is '+JSON.stringify(result));
+      user = result;
+      console.log('auth achieved activity, result:'+JSON.stringify(result));
+      res.render('activity', {title:'Add Activity'});
     }
   });
-  res.render('activity', {title:'Add Activity'});
 };
 
 exports.listActivities = function(req,res){
-  //insert into mongo db
-  Activity.find(function(err, activities){
-    res.send(activities);
-  });
+   if(!user){
+    console.log('auth failed list activities, error:'+JSON.stringify(error));
+    res.render('login', {title: 'Authentication Failed'});
+  } else {
+    console.log('auth list activities achieved, result:'+JSON.stringify(user));
+    Activity.find({user:user.email},function(err, activities){
+      res.send(activities);
+    });
+  }
 };
 
 exports.authenticate = function(req,res){
-  console.log("auth params:"+JSON.stringify(req.query))
-  console.log("relying party is:"+JSON.stringify(relyingParty))
   var identifier = req.query['openid_identifier'];
-  console.log("identifier is:"+JSON.stringify(identifier))
   relyingParty.authenticate(identifier, false, function(error, authUrl){
     if(error){
       res.writeHead(200);
@@ -75,23 +77,27 @@ exports.authenticate = function(req,res){
 
 exports.addActivity = function(req,res){
   //Parse string
-  console.log(req.body.activity);
-  
-  //Parse out the tags
-  var tagpat = /#(\w+)/g;
-  var tagMatches = req.body.activity.match(tagpat);
+    if(!user){
+      console.log('add activity auth failed, error:'+JSON.stringify(error));
+      res.render('login', {title: 'Authentication Failed'});
+    } else {
+      console.log('add activity auth achieved, result:'+JSON.stringify(user));
+      //Parse out the tags
+      var tagpat = /#(\w+)/g;
+      var tagMatches = req.body.activity.match(tagpat);
 
-  var timespent = 0;
+      var timespent = 0;
 
-  //Locate time hints
-  var hourspat = /(\d) hour[s]?/;
-  var timeMatches = req.body.activity.match(hourspat);
-  var time = 0;
+      //Locate time hints
+      var hourspat = /(\d) hour[s]?/;
+      var timeMatches = req.body.activity.match(hourspat);
+      var time = 0;
 
-  if(timeMatches != null){
-    time+=timeMatches[1];
-  }
+      if(timeMatches != null){
+        time+=timeMatches[1];
+      }
 
-  new Activity({activity: req.body.activity, user:"alex", tags: tagMatches, hours:time}).save();
-  res.send("ok");
+      new Activity({activity: req.body.activity, user:user.email, tags: tagMatches, hours:time}).save();
+      res.send("ok");
+    }
 };
